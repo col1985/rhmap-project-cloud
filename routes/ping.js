232a230e1f2e80146b4bbe2ad80b1194b39f5154
Colum Bennett <cbennett@redhat.com>
2017-02-22 17:09:48 +0000

    log headers

--- routes/ping.js
+++ routes/ping.js
@@ -13,7 +13,7 @@ function pingRoute() {
 
     // GET REST endpoint - query params may or may not be populated
     ping.get('/', function(req, res) {
-        console.dir(req)
+        console.dir(req.headers)
         log.info('In ping route GET / req.query=', req.query);
         var path = req.query && req.query.path ? req.query.path : '/ping';
 
